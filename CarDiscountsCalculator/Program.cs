f7b76a09db5aa28a02ccd38f7e9591da5caf9280
ztyszka <zu.tyszka@gmail.com>
2021-11-21 13:53:07 +0100

    firdt commit

---
+++ CarDiscountsCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDiscountsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
