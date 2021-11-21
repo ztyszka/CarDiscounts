679dc6f98f95fd5cc930ee56d640c3e7f050705e
ztyszka <zu.tyszka@gmail.com>
2021-11-21 23:24:39 +0100

    dodany projekt do testów

---
+++ CarDiscountsCalculatorTests/BusinessCarTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using CarDiscountsCalculator;
+
+namespace CarDiscountsCalculatorTests
+{
+    public class BusinessCarTests
+    {
+
+        BusinessCar car;
+        [SetUp]
+        public void Setup()
+        {
+            car = new BusinessCar(3, "Saab", "93", 25000, 10);
+        }
+
+        [Test]
+        public void CarIdTestIsEqual()
+        {
+            Assert.AreEqual(3, car.CarID);
+        }
+
+        [Test]
+        public void CarBrandIsEqual()
+        {
+            Assert.AreEqual("Saab", car.CarBrand);
+        }
+
+        [Test]
+        public void CarBrandIsNotEqual()
+        {
+            Assert.AreNotEqual("Toyota", car.CarBrand);
+        }
+        [Test]
+        public void CarDiscountSetToAcceptableValue()
+        {
+            car.DiscountPercentage = 50;
+            Assert.AreEqual(50, car.DiscountPercentage);
+        }
+        public void CarDiscountSetToGreaterThan100()
+        {
+            car.DiscountPercentage = 120;
+            Assert.AreNotEqual(120, car.DiscountPercentage);
+        }
+    }
+}
---
+++ CarDiscountsCalculatorTests/CarsDatabaseMapperTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using CarDiscountsCalculator;
+using CarDiscountsDatabase;
+using System.Collections.Generic;
+
+namespace CarDiscountsCalculatorTests
+{
+
+    class CarsDatabaseMapperTests
+    {
+        CarsDatabaseMapper mapper;
+        [SetUp]
+        public void Setup()
+        {
+            List<CarDiscountsDatabase.IDatabaseCar> cars = new List<CarDiscountsDatabase.IDatabaseCar>();
+            cars.Add(new CarDiscountsDatabase.DatabaseCar("Toyota", "Auris", 25000));
+            mapper = new CarsDatabaseMapper(cars);
+        }
+
+        [Test]
+        public void CarsDatabaseCreatedCorrectly()
+        {
+            Dictionary<int, BusinessCar> carsDatabase = new Dictionary<int, BusinessCar>();
+            carsDatabase.Add(0, new BusinessCar(0, "Toyota", "Auris", 25000, 0));
+            Assert.AreEqual(carsDatabase, mapper.CarsDatabase);
+            // test się wywala, bo porównuję wskaźniki na struktury, a nie ich wartości
+        }
+    }
+}
